a476013dd296a6ab50749f346dee51a543b697f7
blackleg <hectorespertpardo@gmail.com>
2017-07-18 13:15:33 +0200

    Sending tweet

--- spainmeterbot.py
+++ spainmeterbot.py
@@ -1,4 +1,4 @@
-#import twitter
+from twitter import Api
 from reescraper import IberianPeninsula, BalearicIslands, CanaryIslands
 from arrow import get
 from dotenv import load_dotenv, find_dotenv
@@ -7,7 +7,7 @@ load_dotenv(find_dotenv())
 
 print("### Spain Meter Bot ###")
 
-print("=> Get Peninsula Data")
+print("=> Getting Peninsula Data")
 peninsula = IberianPeninsula().get()
 if not peninsula:
     print("- !! No Peninsula response ¡¡")
@@ -18,10 +18,10 @@ day = date.format('DD', 'es_ES')
 month = date.format('MMMM', 'es_ES')
 time = date.format('HH:mm', 'es_ES')
 
-peninsula_tweet = "Demanda el {0} de {1} a las {2}, Península: {3} MW".format(day, month, time, peninsula.demand)
+peninsula_tweet = "Demanda electricidad España el {0} de {1} a las {2}, Península: {3} MW".format(day, month, time, peninsula.demand)
 
+print("=> Getting Balearic Islands Data")
 balearic = BalearicIslands().get()
-
 if not balearic:
     print("- !! No Balearic Islands response ¡¡")
     balearic_tweet = ''
@@ -30,6 +30,7 @@ else:
     time = balearic_date.format('HH:mm', 'es_ES')
     balearic_tweet = ', Baleares ({0}): {1} MW'.format(time, balearic.demand)
 
+print("=> Getting Canary Islands Data")
 canary = CanaryIslands().get()
 if not canary:
     print("- !! No Canary Islands response ¡¡")
@@ -44,15 +45,16 @@ tweet = peninsula_tweet + balearic_tweet + canary_tweet
 print("=> Generated Tweet: " + tweet)
 
 enabled = environ.get("ENABLED", 'False')
-if enabled == 'False':
+if enabled != 'True':
     print("- !! Send tweet disabled ¡¡")
 else:
-    print("True")
+    print("=> Sending tweet")
+    api = Api(consumer_key=environ.get("CONSUMER_KEY"),
+              consumer_secret=environ.get("CONSUMER_SECRET"),
+              access_token_key=environ.get("ACCESS_KEY"),
+              access_token_secret=environ.get("ACCESS_SECRET"))
+    api.PostUpdate(tweet)
 
-#api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='', access_token_secret='')
-
-#status = api.PostUpdate(tweet)
-#print(status.text)
 
 
 
