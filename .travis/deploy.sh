eaf83643e75ab56da1d1b8c6bd2b1d1e0b47c263
blackleg <hectorespertpardo@gmail.com>
2017-07-18 21:26:42 +0200

    Add deploy script

---
+++ .travis/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "- Deploy Script"
+echo "-- Decrypt ssh key"
+openssl aes-256-cbc -K $encrypted_0b3c30bbe660_key -iv $encrypted_0b3c30bbe660_iv -in deploy_rsa.enc -out deploy_rsa -d
+echo "-- Launch ssh agent"
+eval "$(ssh-agent -s)"
+sleep 5
+echo "-- Add ssh key"
+chmod 600 deploy_rsa
+ssh-add deploy_rsa
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ "$deployurl"
